88b46ea553166aed7e4e586579e8f1ca9f955bd1
Daniel Holmes <daniel@danielholmes.org>
2020-02-03 14:24:11 +1100

    Get working for gatsby sites in repo sub directories

--- src/make-relative.js
+++ src/make-relative.js
@@ -6,14 +6,17 @@ const cwd = process.cwd()
 
 module.exports = async (markdownPath, imagePath, options) => {
 	const { mediaPath, publicPath } = await getConfig(options)
+	const mediaSlashPath = '/' + mediaPath + '/';
 	if(
 		typeof imagePath !== `string` ||
-		imagePath.indexOf(`${publicPath}/`) !== 0
+		imagePath.indexOf(mediaSlashPath) !== 0
 	){
 		return imagePath
 	}
-	markdownPath = dirname(markdownPath).replace(`${cwd}/`, ``)
-	imagePath = imagePath.replace(publicPath, mediaPath)
-	const newPath = relative(markdownPath, imagePath)
+
+	const mediaStartSlashPath = '/' + mediaPath
+	markdownPath = dirname(markdownPath).replace(cwd + "/", "");
+	imagePath = imagePath.replace(mediaStartSlashPath, publicPath).substring(1);
+	const newPath = relative(markdownPath, imagePath);
 	return slash(newPath)
 }
