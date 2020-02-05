86cb3aa804ea4cfd82d380ca23e321adc658537b
Daniel Holmes <daniel@danielholmes.org>
2020-02-05 13:43:17 +1100

    Try and fix for relative paths

--- src/make-relative.js
+++ src/make-relative.js
@@ -6,17 +6,16 @@ const cwd = process.cwd()
 
 module.exports = async (markdownPath, imagePath, options) => {
 	const { mediaPath, publicPath } = await getConfig(options)
-	const mediaSlashPath = '/' + mediaPath + '/';
 	if(
 		typeof imagePath !== `string` ||
-		imagePath.indexOf(mediaSlashPath) !== 0
+		imagePath.indexOf(publicPath) !== 0
 	){
 		return imagePath
 	}
 
-	const mediaStartSlashPath = '/' + mediaPath
+	const mediaSlashPath = '/' + mediaPath
 	markdownPath = dirname(markdownPath).replace(cwd + "/", "");
-	imagePath = imagePath.replace(mediaStartSlashPath, publicPath).substring(1);
+	imagePath = imagePath.replace(mediaSlashPath, publicPath).substring(1);
 	const newPath = relative(markdownPath, imagePath);
 	return slash(newPath)
 }
